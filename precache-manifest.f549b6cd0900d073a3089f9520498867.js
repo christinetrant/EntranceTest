f324d125cd40e904fd6c1f903172502d1ee0df6b
Christine Trant <christinetrant@gmail.com>
2020-09-10 15:19:28 +0100

    Updates

---
+++ precache-manifest.f549b6cd0900d073a3089f9520498867.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37692f92a1d8455b63b363be5ccfa4e0",
+    "url": "/EntranceTest/index.html"
+  },
+  {
+    "revision": "95f01e40121d732b5a6c",
+    "url": "/EntranceTest/static/css/main.fcdf0155.chunk.css"
+  },
+  {
+    "revision": "5526380ab317a6222491",
+    "url": "/EntranceTest/static/js/2.87123d3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/EntranceTest/static/js/2.87123d3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95f01e40121d732b5a6c",
+    "url": "/EntranceTest/static/js/main.fc855689.chunk.js"
+  },
+  {
+    "revision": "fe296927bd398e2950ae",
+    "url": "/EntranceTest/static/js/runtime-main.864ce96e.js"
+  },
+  {
+    "revision": "f850825c3915eaa4ec75bc86c31287b3",
+    "url": "/EntranceTest/static/media/btn-1.f850825c.svg"
+  },
+  {
+    "revision": "3cf0799a16709002e6ee30de2a215d0e",
+    "url": "/EntranceTest/static/media/btn-2.3cf0799a.svg"
+  },
+  {
+    "revision": "33f5be3849a1bb8195f0d6716984201b",
+    "url": "/EntranceTest/static/media/btn-3.33f5be38.svg"
+  },
+  {
+    "revision": "9f726fba48cb47096e2cbe4432c4381c",
+    "url": "/EntranceTest/static/media/btn-4.9f726fba.svg"
+  },
+  {
+    "revision": "bafa3fa0893428d22bc7d201b52eefc4",
+    "url": "/EntranceTest/static/media/btn-5.bafa3fa0.svg"
+  },
+  {
+    "revision": "07cce7b4ac6be5bb3ee5f5fa387a4916",
+    "url": "/EntranceTest/static/media/btn-6.07cce7b4.svg"
+  },
+  {
+    "revision": "6984aa41126338d3dd3ac0fa2a73185c",
+    "url": "/EntranceTest/static/media/image.6984aa41.jpg"
+  },
+  {
+    "revision": "042ef0a77edf4ed0949f81b90c3ff71f",
+    "url": "/EntranceTest/static/media/invite.042ef0a7.svg"
+  },
+  {
+    "revision": "57c4f0610bc30ff134bffec5d1853d9e",
+    "url": "/EntranceTest/static/media/logo.57c4f061.svg"
+  },
+  {
+    "revision": "9139a3e1bb98e6eb35739fbe74115425",
+    "url": "/EntranceTest/static/media/workboard.9139a3e1.svg"
+  }
+]);
